2fa37be721203226e8bba24bb3e1dced68ed9d92
Samuel Porto <samuelalcala2001@outlook.com>
2023-01-25 16:26:48 -0300

    feat: captura dos dados do formulário e enviando para ao model

--- controllers/products.go
+++ controllers/products.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 
 	"github.com/Porto-08/app-web-crud/models"
 )
@@ -20,4 +21,28 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func AddProduct(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "AddProduct", nil)
+}
+
+func InsertProduct(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		name := r.FormValue("nome")
+		description := r.FormValue("descricao")
+		price := r.FormValue("preco")
+		quantity := r.FormValue("quantidade")
+
+		convertPrice, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		convertQuantity, err := strconv.Atoi(quantity)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		models.InsertProduct(name, description, convertPrice, convertQuantity)
+	}
+
+	http.Redirect(w, r, "/", 301)
 }
